2346bee1f3d25a11875a7c24fa35b709c1805dc0
coderslav <coderslav>
2020-12-23 19:18:56 +0100

    Task_1_10_3_and_1_10_4

---
+++ task_1_10_3.py
@@ -0,0 +1,15 @@
+class Client:
+    def __init__(self, first_name, last_name, balance=0.0, city="Не указан", status="Не указан"):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.city = city
+        self.balance = balance
+        self.status = status
+
+    def get_info(self):
+        return f"Клиент <<{self.first_name} {self.last_name}>>. Баланс: {self.balance} руб."
+
+
+if __name__ == '__main__':
+    client_1 = Client("Иван", "Петров", 50)
+    print(client_1.get_info())
---
+++ task_1_10_4.py
@@ -0,0 +1,26 @@
+from task_1_10_3 import Client
+
+
+guests = []
+
+
+class Guests(Client):
+
+    def add_to_guests_list(self):
+        guests.append(self)
+
+    def get_guest_status(self):
+        if self in guests:
+            return f"<<{self.first_name} {self.last_name}>>, г. {self.city}, статус {self.status}"
+        else:
+            return f"<<{self.first_name} {self.last_name}>> не в списке приглашенных"
+
+
+client_1 = Guests("Иван", "Петров", 50, "Москва", '"Наставник"')
+client_1.add_to_guests_list()
+client_2 = Guests("Петр", "Серебренников", city="Рязань")
+client_3 = Guests("Гвидо", "Ван Россум", 1000000, "Калифорния", '"Cоздатель языка программирования Python"')
+client_3.add_to_guests_list()
+print(client_1.get_info(), client_2.get_info(), client_3.get_info(), sep="\n")
+print("V●ᴥ●V V●ᴥ●V V●ᴥ●V V●ᴥ●V V●ᴥ●V V●ᴥ●V V●ᴥ●V V●ᴥ●V V●ᴥ●V V●ᴥ●V V●ᴥ●V V●ᴥ●V V●ᴥ●V V●ᴥ●V V●ᴥ●V V●ᴥ●V V●ᴥ●V V●ᴥ●V ")
+print(client_1.get_guest_status(), client_2.get_guest_status(), client_3.get_guest_status(), sep="\n")
